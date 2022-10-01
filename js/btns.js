b72f4368d7a1587e463e36036334dcb9a5dd3e48
Michael Elisha <45241489+MichaelElisha1988@users.noreply.github.com>
2022-10-01 17:03:27 +0300

    mobile ver PIG

--- js/btns.js
+++ js/btns.js
@@ -1,17 +1,21 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const btnCloseModal = document.querySelector('.close-modal');
+const modalR = document.querySelector('.modal-rules');
+const modalS = document.querySelector('.modal-setting');
+const btnCloseModalR = document.querySelector('.close-modal-rules');
+const btnCloseModalS = document.querySelector('.close-modal-setting');
 const overlay = document.querySelector('.overlay');
 
 const btnsOpenModals = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
+  let modal = this.textContent === 'Rules' ? modalR : modalS;
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
+  let modal = this.classList.contains('close-modal-rules') ? modalR : modalS;
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
@@ -20,9 +24,5 @@ btnsOpenModals.forEach(function (btn, i) {
   btn.addEventListener('click', openModal);
 });
 
-btnCloseModal.addEventListener('click', closeModal);
-document.addEventListener('keydown', function (e) {
-  if (e.key == 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
+btnCloseModalR.addEventListener('click', closeModal);
+btnCloseModalS.addEventListener('click', closeModal);
